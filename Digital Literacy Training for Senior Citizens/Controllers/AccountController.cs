ecd308f446bd143eac646a080aa73fcd2f5ef472
nidhi <mishranidhi664-gif>
2026-09-17 23:49:55 +0530

    Add Google Analytics tracking

---
+++ Digital Literacy Training for Senior Citizens/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Digital_Literacy_Training_for_Senior_Citizens.Models;
+
+namespace Digital_Literacy_Training_for_Senior_Citizens.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly PasswordHasher<User> _passwordHasher;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+            _passwordHasher = new PasswordHasher<User>();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user, string ConfirmPassword)
+        {
+            if (user.Password != ConfirmPassword)
+            {
+                ViewBag.Error = "Passwords do not match.";
+                return View(user);
+            }
+
+            user.Password = _passwordHasher.HashPassword(user, user.Password);
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
